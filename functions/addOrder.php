0fb961c450a23a7b02258e2da092661552788979
s15007 <s15007@std.it-college.ac.jp>
2017-06-27 13:53:54 +0900

    commit

--- functions/addOrder.php
+++ functions/addOrder.php
@@ -14,7 +14,6 @@ foreach ($cartData as $key => $list) {
             'insert into `order`(order_date, user_id, product_id, product_num, tray_id, tray_num, rootstock_id, receiving_date) 
             VALUES (sysdate(), "'.$_SESSION['userid'].'", "'.$list['product_id'].'", "'.$list['product_num'].'", "'.$list['tray_id'].'", "300", "'.$list['rootstock_id'].'", "'.$list['receiving_date'].'")'
         );
-        $stmt->execute();
     } catch (PDOException $pdoe) {
         echo $pdoe->getMessage();
     }
@@ -27,8 +26,22 @@ try {
         'TRUNCATE TABLE `cart`'
     );
     $stmt->execute();
-    header("Location: ../home.php");
+//    header("Location: ../home.php");
 } catch (PDOException $pdoe) {
     echo $pdoe->getMessage();
 }
-
+$orderData = getOrderData($_SESSION['userid']);
+//
+//foreach ($orderData as $key => $list) {
+//    try {
+//        $dbh = new PDO(DSN, DB_USER, DB_PWD);
+//        $dbh->query('SET NAMES UTF8');
+//        $stmt = $dbh->prepare(
+//            VALUES ()'
+//        );
+//        $stmt'INSERT INTO seeding(seeding_date, order_id, status)
+//             ->execute();
+//    } catch (PDOException $pdoe) {
+//        echo $pdoe->getMessage();
+//    }
+//}
